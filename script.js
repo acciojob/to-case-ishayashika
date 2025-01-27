d40c418458509d007bbfd074355e043144e58119
ishayashika <122111684+ishayashika@users.noreply.github.com>
2025-01-28 01:10:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function toCase(text) {
   // write your code here
-	return text ? text.toLowerCase() + "-" + text.toUpperCase() : null;
+	return text ? text.toLowerCase() + "-" + text.toUpperCase() : "-";
 }
+
 
 
 
